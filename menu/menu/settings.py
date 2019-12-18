912d83660635c67798ce694d0b7ef3833fbc83c8
arsummers <aliyasummers1@gmail.com>
2019-12-17 18:22:10 -0800

    first migrations, nothing broken

--- menu/menu/settings.py
+++ menu/menu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dishes.apps.DishesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Set up to take images into database
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
